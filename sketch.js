b729c5567528e18cf18126480f9a26eb4071c173
Ashton Yeackley <aey21@fsu.edu>
2022-11-21 12:49:38 -0500

    got plastic bottles working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,48 @@
-function setup() {
-  createCanvas(400, 400);
+let shore;
+let wave;
+let bottle;
+let plastic;
+let glass;
+let clean = 0;
+let plastic1 = true;
+let plastic2 = true;
+let plastic3 = true;
+
+
+function preload() {
+  shore = loadImage('assets/shore.png');
+  //wave = loadImage('wave.png');
+  plastic = loadImage('assets/plastic.png');
+  glass = loadImage('assets/glass.png')
 }
 
-function draw() {
-  background(125);
+function setup() {
+  createCanvas(800, 600);
 }
+
+function draw() { 
+  image(shore,0,0);
+
+  if (plastic1){
+    image(plastic,110,235); }
+  if (plastic2){
+    image(plastic,455,455); }
+  if (plastic3){
+    image(plastic,710,235); }
+}
+
+function mousePressed() {
+  if (collidePointRect(mouseX,mouseY,110,235,50,130)) {
+    plastic1 = false;
+    clean++;
+  }
+  if (collidePointRect(mouseX,mouseY,455,455,50,130)){
+    plastic2 = false;
+    clean++;
+  }
+  if (collidePointRect(mouseX,mouseY,710,235,50,130)){
+    plastic3 = false;
+    clean++;
+  }
+}
+
